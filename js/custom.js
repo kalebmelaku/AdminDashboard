31a2a58b0cdcf74350e8f40ae703508e7fe940b3
kayoJr <kalebmelaku97@gmail.com>
2023-03-10 15:32:07 +0300

    darkmode lightmode switch added

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,29 @@
 const icon = document.getElementById('icon_switch');
-let icon_stat = true;
-let icon_src;
-icon.addEventListener('click', () => {
-    icon_stat = !icon_stat;
-    icon_stat ? icon_src = '../img/icons/moon.png' : icon_src = '../img/icons/sun.png';
-    icon.src = icon_src;
-    if (document.body.classList.contains('light')) {
-        document.body.classList.remove('light');
+document.addEventListener("DOMContentLoaded", () => {
+    let status = sessionStorage.getItem("colorMode");
+    // document.body.classList.remove('')
+    if (status == 'dark') {   
+        document.body.classList.remove('light')
+        icon.src = '../img/icons/sun.png';
     } else {
-        document.body.classList.add('light');
+        document.body.classList.add(status)
     }
 })
+
+// let icon_stat = true;
+// let icon_src;
+icon.addEventListener('click', () => {
+    // icon_stat = !icon_stat;
+    // icon_stat ? icon_src = '../img/icons/moon.png' : icon_src = '../img/icons/sun.png';
+    // icon.src = icon_src;
+    if (document.body.classList.contains('light')) {
+        document.body.classList.remove('light');
+        icon.src = '../img/icons/sun.png';
+        sessionStorage.setItem("colorMode", "dark");
+    } else {
+        sessionStorage.setItem("colorMode", "light")
+        icon.src = '../img/icons/moon.png';
+        document.body.classList.add('light');
+    }
+});
+
